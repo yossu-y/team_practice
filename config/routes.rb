9efcecbdd13bd71848abba63b340de1851ea2575
yossu-y <yangyuub@gmail.com>
2022-05-16 15:21:40 +0000

    [update]cart_item

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
 
+  scope module: :public do
+    resources :cart_items, only: [:index, :update, :create, :destroy]
+  end
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :admin do
+  end
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
