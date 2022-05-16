eeba270fb32c09abb9df8c1cc4666ca6985da726
yossu-y <yangyuub@gmail.com>
2022-05-16 15:08:26 +0000

    [update]devise-views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
+
+  root to: "public/homes#top"
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
