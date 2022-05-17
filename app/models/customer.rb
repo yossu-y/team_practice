3d9f2bcf3f7dfac9da7561668c6aa0dc9d01996a
yossu-y <yangyuub@gmail.com>
2022-05-17 02:54:36 +0000

    [add]customer.genre-バリテーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,4 +9,12 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
 end
